23ef7821ae13c086fd1f2211c7811b979922251b
magzu <magzu90@gmail.com>
2019-08-03 15:20:11 +0200

    Update AOUI.sh

--- AOUI.sh
+++ AOUI.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIRECTORY=`pwd`
 HEIGHT=15
 WIDTH=60
 CHOICE_HEIGHT=2
@@ -27,10 +28,18 @@ CHOICE_HEIGHT=4
 
 dialog --checklist "Choose from following (spacebar to select)" 0 0 0 \
 Clicksaver "Tool to look for missions" off \
+MishBuddy "Alternative to ClickSaver" off \
 RKMap "Improved Rubi-Ka Planet Map" off \
 SLMap "Improved ShadowLands Planet Map" off \
 TinyDump "WIP - damage dumper to see how much damage you do" off \
 2> /var/tmp/optional.out
+if [ "$?" != "0" ]
+then
+	clear
+	echo "cancel pressed. Exiting script."
+	exit
+fi
+
 OPTIONAL=`cat /var/tmp/optional.out | \
     sed -e "s/\"//g" -e "s/ /|/g" -e "s/|$//"`
     echo 'optional :'$OPTIONAL
@@ -40,30 +49,74 @@ folder="$(zenity --file-selection --directory --title="Choose install directory"
 clear
 echo 10 | dialog --title "Installing" --gauge "Creating wineprefix" 10 75 &
 
-WINEPREFIX=$folder WINEARCH=win32 winetricks winxp > /dev/null
+WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 winetricks winxp > /dev/null
 cd $folder
 clear
 echo 20 | dialog --title "Installing" --gauge "Downloading files" 10 75 &
 
 mkdir dl
 cd dl
-curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP1.exe > /dev/null
+curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP1.exe 2> /dev/null
     
     
 #ThirdParty
 	if [[ $OPTIONAL == *"Clicksaver"* ]]; then
   		#echo "Clicksaver Selected" 
-  		curl -O http://singsrv.magzu.net/CS240.zip > /dev/null
+  		clear
+		echo 21 | dialog --title "Installing" --gauge "Downloading Clicksaver." 10 75 &
+  		curl -O http://singsrv.magzu.net/CS240.zip 2> /dev/null
+  		clear
+		echo 22 | dialog --title "Installing" --gauge "Installing Clicksaver." 10 75 &
   		mkdir ../drive_c/Clicksaver/
-  		unzip CS240.zip -d ../drive_c/Clicksaver/
+  		unzip CS240.zip -d ../drive_c/Clicksaver/ 2> /dev/null
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartClicksaver.sh
+  		echo "cd $folder/drive_c/Clicksaver/" >> $DIRECTORY/StartClicksaver.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine ClickSaver.exe" >> $DIRECTORY/StartClicksaver.sh
+		chmod +x $DIRECTORY/StartClicksaver.sh
+	fi
+	if [[ $OPTIONAL == *"MishBuddy"* ]]; then
+  		#echo "MishBuddy Selected"
+  		clear
+		echo 23 | dialog --title "Installing" --gauge "Downloading MishBuddy." 10 75 &
+  		curl -O curl -O https://www.mmotoolbox.com/download/mbsetup.exe 2> /dev/null
+  		clear
+		echo 24 | dialog --title "Installing" --gauge "Installing MishBuddy. Please Select Defaults" 10 75 &
+		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine mbsetup.exe
+		
+		#Shortcut
+		echo "#!/bin/bash" > $DIRECTORY/StartMishBuddy.sh
+  		echo "cd $folder/drive_c/Program\ Files/MMOToolbox/MishBuddy/" >> $DIRECTORY/StartMishBuddy.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine MishBuddy.exe" >> $DIRECTORY/StartMishBuddy.sh
+		chmod +x $DIRECTORY/StartMishBuddy.sh
 	fi
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		#echo "Bitnykk's SLmap Selected"
-  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip > /dev/null
+  		#echo "Bitnykk SLmap Selected"
+  		clear
+		echo 25 | dialog --title "Installing" --gauge "Downloading SLmap." 10 75 &
+  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip 2> /dev/null
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
-  		#echo "Savic's RK map Selected"
-  		curl -O http://singsrv.magzu.net/SaavicksMap.zip > /dev/null
+  		#echo "Savic RK map Selected"
+  		clear
+		echo 26 | dialog --title "Installing" --gauge "Downloading SaavicksMap." 10 75 &
+  		curl -O http://singsrv.magzu.net/SaavicksMap.zip 2> /dev/null
+	fi
+	if [[ $OPTIONAL == *"TinyDump"* ]]; then
+  		#echo "TinyDump selected"
+  		clear
+		echo 27 | dialog --title "Installing" --gauge "Downloading TinyDump." 10 75 &
+  		curl -O http://tinydump.adams1.de/dateien/SetupTinyDump_1_1_1.exe 2> /dev/null
+  		clear
+		echo 28 | dialog --title "Installing" --gauge "Installing TinyDump." 10 75 &
+  		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine SetupTinyDump_1_1_1.exe /silent
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartTinyDump.sh
+  		echo "cd $folder/drive_c/Program\ Files/TinyDump/" >> $DIRECTORY/StartTinyDump.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine TinyDump.exe" >> $DIRECTORY/StartTinyDump.sh
+		chmod +x $DIRECTORY/StartTinyDump.sh
 	fi
 
 clear
@@ -73,14 +126,24 @@ WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline_EP1.exe
 clear
 echo 95 | dialog --title "Installing" --gauge "Installing addons" 10 75 &
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ > /dev/null
+  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ 2> /dev/null
   		
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
   		rm ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/*
-  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ > /dev/null
+  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ 2> /dev/null
   		
 	fi
+clear
+echo 99 | dialog --title "Installing" --gauge "Creating shortcuts" 10 75 &
+
+cd $DIRECTORY
+echo "#!/bin/bash" > StartAO.sh
+echo "cd $folder/drive_c/Funcom/Anarchy\ Online/" >> StartAO.sh
+echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline.exe" >> StartAO.sh
+chmod +x StartAO.sh
+
+
 clear
 echo 100 | dialog --title "Installing" --gauge "Done" 10 75 &
 sleep 3
@@ -97,7 +160,7 @@ echo installation is complete.
 
         2)
 TITLE="Installation - New Engine"
-MENU="Which version?:"
+MENU="Which version?"
 
 OPTIONS=(1 "Wined3d - OpenGL"
          2 "d9vk - Vulkan")
@@ -119,20 +182,27 @@ echo wined3d
 
 dialog --checklist "Choose from following (spacebar to select)" 0 0 0 \
 Clicksaver "Tool to look for missions" off \
+MishBuddy "Alternative to ClickSaver" off \
 RKMap "Improved Rubi-Ka Planet Map" off \
 SLMap "Improved ShadowLands Planet Map" off \
 TinyDump "WIP - damage dumper to see how much damage you do" off \
 2> /var/tmp/optional.out
+if [ "$?" != "0" ]
+then
+	clear
+	echo "cancel pressed. Exiting script."
+	exit
+fi
+
 OPTIONAL=`cat /var/tmp/optional.out | \
     sed -e "s/\"//g" -e "s/ /|/g" -e "s/|$//"`
     echo 'optional :'$OPTIONAL
-
 
 folder="$(zenity --file-selection --directory --title="Choose install directory")"
 clear
 echo 10 | dialog --title "Installing" --gauge "Creating wineprefix" 10 75 &
 
-WINEPREFIX=$folder WINEARCH=win32 winetricks d3dcompiler_43 d3dx9 > /dev/null
+WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 winetricks d3dcompiler_43 d3dx9 > /dev/null
 
 cd $folder
 clear
@@ -140,22 +210,66 @@ echo 20 | dialog --title "Installing" --gauge "Downloading files" 10 75 &
 
 mkdir dl
 cd dl
-curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP2.exe > /dev/null
+curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP2.exe 2> /dev/null
 
 #ThirdParty
 	if [[ $OPTIONAL == *"Clicksaver"* ]]; then
   		#echo "Clicksaver Selected" 
-  		curl -O http://singsrv.magzu.net/CS240.zip > /dev/null
+  		clear
+		echo 21 | dialog --title "Installing" --gauge "Downloading Clicksaver." 10 75 &
+  		curl -O http://singsrv.magzu.net/CS240.zip 2> /dev/null
+  		clear
+		echo 22 | dialog --title "Installing" --gauge "Installing Clicksaver." 10 75 &
   		mkdir ../drive_c/Clicksaver/
-  		unzip CS240.zip -d ../drive_c/Clicksaver/
+  		unzip CS240.zip -d ../drive_c/Clicksaver/ 2> /dev/null
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartClicksaver.sh
+  		echo "cd $folder/drive_c/Clicksaver/" >> $DIRECTORY/StartClicksaver.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine ClickSaver.exe" >> $DIRECTORY/StartClicksaver.sh
+		chmod +x $DIRECTORY/StartClicksaver.sh
+	fi
+	if [[ $OPTIONAL == *"MishBuddy"* ]]; then
+  		#echo "MishBuddy Selected"
+  		clear
+		echo 23 | dialog --title "Installing" --gauge "Downloading MishBuddy." 10 75 &
+  		curl -O curl -O https://www.mmotoolbox.com/download/mbsetup.exe 2> /dev/null
+  		clear
+		echo 24 | dialog --title "Installing" --gauge "Installing MishBuddy. Please Select Defaults" 10 75 &
+		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine mbsetup.exe
+		
+		#Shortcut
+		echo "#!/bin/bash" > $DIRECTORY/StartMishBuddy.sh
+  		echo "cd $folder/drive_c/Program\ Files/MMOToolbox/MishBuddy/" >> $DIRECTORY/StartMishBuddy.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine MishBuddy.exe" >> $DIRECTORY/StartMishBuddy.sh
+		chmod +x $DIRECTORY/StartMishBuddy.sh
 	fi
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		#echo "Bitnykk's SLmap Selected"
-  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip > /dev/null
+  		#echo "Bitnykk SLmap Selected"
+  		clear
+		echo 25 | dialog --title "Installing" --gauge "Downloading SLmap." 10 75 &
+  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip 2> /dev/null
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
-  		#echo "Savic's RK map Selected"
-  		curl -O http://singsrv.magzu.net/SaavicksMap.zip > /dev/null
+  		#echo "Savic RK map Selected"
+  		clear
+		echo 26 | dialog --title "Installing" --gauge "Downloading SaavicksMap." 10 75 &
+  		curl -O http://singsrv.magzu.net/SaavicksMap.zip 2> /dev/null
+	fi
+	if [[ $OPTIONAL == *"TinyDump"* ]]; then
+  		#echo "TinyDump selected"
+  		clear
+		echo 27 | dialog --title "Installing" --gauge "Downloading TinyDump." 10 75 &
+  		curl -O http://tinydump.adams1.de/dateien/SetupTinyDump_1_1_1.exe 2> /dev/null
+  		clear
+		echo 28 | dialog --title "Installing" --gauge "Installing TinyDump." 10 75 &
+  		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine SetupTinyDump_1_1_1.exe /silent
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartTinyDump.sh
+  		echo "cd $folder/drive_c/Program\ Files/TinyDump/" >> $DIRECTORY/StartTinyDump.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine TinyDump.exe" >> $DIRECTORY/StartTinyDump.sh
+		chmod +x $DIRECTORY/StartTinyDump.sh
 	fi
 clear
 echo 30 | dialog --title "Installing" --gauge "Installing game. Please select defaults" 10 75 &
@@ -164,14 +278,23 @@ WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline_EP2.exe
 clear
 echo 95 | dialog --title "Installing" --gauge "Installing addons" 10 75 &
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ > /dev/null
+  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ 2> /dev/null
   		
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
   		rm ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/*
-  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ > /dev/null
+  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ 2> /dev/null
   		
 	fi
+clear
+echo 99 | dialog --title "Installing" --gauge "Creating shortcuts" 10 75 &
+
+cd $DIRECTORY
+echo "#!/bin/bash" > StartAO.sh
+echo "cd $folder/drive_c/Funcom/Anarchy\ Online/" >> StartAO.sh
+echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline.exe" >> StartAO.sh
+chmod +x StartAO.sh
+
 clear
 echo 100 | dialog --title "Installing" --gauge "Done" 10 75 &
 sleep 3
@@ -184,15 +307,21 @@ echo d9vk
 
 dialog --checklist "Choose from following (spacebar to select)" 0 0 0 \
 Clicksaver "Tool to look for missions" off \
+MishBuddy "Alternative to ClickSaver" off \
 RKMap "Improved Rubi-Ka Planet Map" off \
 SLMap "Improved ShadowLands Planet Map" off \
 TinyDump "WIP - damage dumper to see how much damage you do" off \
 2> /var/tmp/optional.out
+if [ "$?" != "0" ]
+then
+	clear
+	echo "cancel pressed. Exiting script."
+	exit
+fi
+
 OPTIONAL=`cat /var/tmp/optional.out | \
     sed -e "s/\"//g" -e "s/ /|/g" -e "s/|$//"`
     echo 'optional :'$OPTIONAL
-	
-
 
 
 #New Engine - D9VK
@@ -201,7 +330,7 @@ folder="$(zenity --file-selection --directory --title="Choose install directory"
 clear
 echo 10 | dialog --title "Installing" --gauge "Creating wineprefix" 10 75 &
 
-WINEPREFIX=$folder WINEARCH=win32 winetricks d3dcompiler_43 d3dx9 d9vk_master > /dev/null
+WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 winetricks d3dcompiler_43 d3dx9 d9vk_master > /dev/null
 
 cd $folder
 clear
@@ -209,22 +338,66 @@ echo 20 | dialog --title "Installing" --gauge "Downloading files" 10 75 &
 
 mkdir dl
 cd dl
-curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP2.exe > /dev/null
+curl -O http://update.anarchy-online.com/download/AO/AnarchyOnline_EP2.exe 2> /dev/null
 
 #ThirdParty
 	if [[ $OPTIONAL == *"Clicksaver"* ]]; then
   		#echo "Clicksaver Selected" 
-  		curl -O http://singsrv.magzu.net/CS240.zip > /dev/null
+  		clear
+		echo 21 | dialog --title "Installing" --gauge "Downloading Clicksaver." 10 75 &
+  		curl -O http://singsrv.magzu.net/CS240.zip 2> /dev/null
+  		clear
+		echo 22 | dialog --title "Installing" --gauge "Installing Clicksaver." 10 75 &
   		mkdir ../drive_c/Clicksaver/
-  		unzip CS240.zip -d ../drive_c/Clicksaver/
+  		unzip CS240.zip -d ../drive_c/Clicksaver/ 2> /dev/null
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartClicksaver.sh
+  		echo "cd $folder/drive_c/Clicksaver/" >> $DIRECTORY/StartClicksaver.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine ClickSaver.exe" >> $DIRECTORY/StartClicksaver.sh
+		chmod +x $DIRECTORY/StartClicksaver.sh
+	fi
+	if [[ $OPTIONAL == *"MishBuddy"* ]]; then
+  		#echo "MishBuddy Selected"
+  		clear
+		echo 23 | dialog --title "Installing" --gauge "Downloading MishBuddy." 10 75 &
+  		curl -O curl -O https://www.mmotoolbox.com/download/mbsetup.exe 2> /dev/null
+  		clear
+		echo 24 | dialog --title "Installing" --gauge "Installing MishBuddy. Please Select Defaults" 10 75 &
+		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine mbsetup.exe
+		
+		#Shortcut
+		echo "#!/bin/bash" > $DIRECTORY/StartMishBuddy.sh
+  		echo "cd $folder/drive_c/Program\ Files/MMOToolbox/MishBuddy/" >> $DIRECTORY/StartMishBuddy.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine MishBuddy.exe" >> $DIRECTORY/StartMishBuddy.sh
+		chmod +x $DIRECTORY/StartMishBuddy.sh
 	fi
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		#echo "Bitnykk's SLmap Selected"
-  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip > /dev/null
+  		#echo "Bitnykk SLmap Selected"
+  		clear
+		echo 25 | dialog --title "Installing" --gauge "Downloading SLmap." 10 75 &
+  		curl -O https://rubi-ka.net/bitnykk/SLmap-v2.1-normal.zip 2> /dev/null
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
-  		#echo "Savic's RK map Selected"
-  		curl -O http://singsrv.magzu.net/SaavicksMap.zip > /dev/null
+  		#echo "Savic RK map Selected"
+  		clear
+		echo 26 | dialog --title "Installing" --gauge "Downloading SaavicksMap." 10 75 &
+  		curl -O http://singsrv.magzu.net/SaavicksMap.zip 2> /dev/null
+	fi
+	if [[ $OPTIONAL == *"TinyDump"* ]]; then
+  		#echo "TinyDump selected"
+  		clear
+		echo 27 | dialog --title "Installing" --gauge "Downloading TinyDump." 10 75 &
+  		curl -O http://tinydump.adams1.de/dateien/SetupTinyDump_1_1_1.exe 2> /dev/null
+  		clear
+		echo 28 | dialog --title "Installing" --gauge "Installing TinyDump." 10 75 &
+  		WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine SetupTinyDump_1_1_1.exe /silent
+  		
+  		#Shortcut
+  		echo "#!/bin/bash" > $DIRECTORY/StartTinyDump.sh
+  		echo "cd $folder/drive_c/Program\ Files/TinyDump/" >> $DIRECTORY/StartTinyDump.sh
+		echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine TinyDump.exe" >> $DIRECTORY/StartTinyDump.sh
+		chmod +x $DIRECTORY/StartTinyDump.sh
 	fi
 clear
 echo 30 | dialog --title "Installing" --gauge "Installing game. Please select defaults" 10 75 &
@@ -233,14 +406,26 @@ WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline_EP2.exe
 clear
 echo 95 | dialog --title "Installing" --gauge "Installing addons" 10 75 &
 	if [[ $OPTIONAL == *"SLMap"* ]]; then
-  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ > /dev/null
+  		unzip SLmap-v2.1-normal.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/ 2> /dev/null
   		
 	fi
 	if [[ $OPTIONAL == *"RKMap"* ]]; then
   		rm ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/*
-  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ > /dev/null
+  		unzip SaavicksMap.zip -d ../drive_c/Funcom/Anarchy\ Online/cd_image/textures/PlanetMap/SaavicksMap/ 2> /dev/null
   		
 	fi
+clear
+echo 99 | dialog --title "Installing" --gauge "Creating shortcuts" 10 75 &
+
+cd $DIRECTORY
+echo "#!/bin/bash" > StartAO.sh
+echo "cd $folder/drive_c/Funcom/Anarchy\ Online/" >> StartAO.sh
+echo "WINEDEBUG=-all WINEPREFIX=$folder WINEARCH=win32 wine AnarchyOnline.exe" >> StartAO.sh
+chmod +x StartAO.sh
+
+
+
+
 clear
 echo 100 | dialog --title "Installing" --gauge "Done" 10 75 &
 sleep 3
