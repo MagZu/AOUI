8b4f1475c139c2bfe66af853f5603ef62bb01e23
magzu <magzu90@gmail.com>
2019-08-12 23:38:57 +0200

    Update AOUI.sh

--- AOUI.sh
+++ AOUI.sh
@@ -363,7 +363,7 @@ create_AO_shortcut () {
 cd $DIRECTORY
 
 if [[ $kernel == *"FreeBSD"* ]]; then
-	echo 'env WINEPREFIX="/home/$USER/Games/Anarchy Online" wine C:\windows\command\start.exe /Unix "/home/$USER/Games/Anarchy Online/dosdevices/c:/users/Public/Desktop/Anarchy\ Online.lnk"'
+	echo 'env WINEPREFIX="$folder" wine C:\windows\command\start.exe /Unix "$folder/dosdevices/c:/users/Public/Desktop/Anarchy\ Online.lnk"'
 fi
 if [[ $kernel == *"Linux"* ]]; then
 echo "#!/bin/bash" > StartAO.sh
